4375436c74d23f21ec0dae7a75e72459b2cdff49
rohit kumar <rohit98077@gmail.com>
2020-09-30 22:20:57 +0530

    some changes

--- mac1.py
+++ mac1.py
@@ -1,7 +1,7 @@
 import argparse
 parser=argparse.ArgumentParser()
-parser.add_argument('--first',help='enter firstname')
-parser.add_argument('--last',help='enter lastname')
+parser.add_argument('--first',help='enter firstname',default="abc")
+parser.add_argument('--last',help='enter lastname',default='xyz')
 args=parser.parse_args()
 print(type(args))
 f=args.first
